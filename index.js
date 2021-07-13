40a4ccb6e901509ca33268f9adf533f10900778b
Jeremymos254 <jeremy.moseti@yahoo.com>
2021-07-13 15:42:48 -0600

    answered question 6

--- index.js
+++ index.js
@@ -136,33 +136,32 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
 let computer =  Math.random();
-if(COMPUTER <= 0.34){
-  COMPUTER = "ROCK";
+
+if (computer <= 0.34){
+  computer ='Rock';
 
 }else if (computer <= 0.67){
-  computer = "paper"
-}else if (computer > 0.67){
-  computer = "Scissors"
+  computer = 'Paper';
+}else if(computer > 0.67) {
+  computer = 'Scissors';
 }
 
 function game(user, computer){
-  /*add your code here*/
 
   if(user === computer){
-    return 'its a tie';
-  }
-}else if(user === 'rock'&& computer === 'Scissors'){
-  return 'you win';
+    return "it's a tie";
 
-}else if (user === 'paper' && computer === 'rock'){
-  return 'you win';
-}else if(user === 'scissors' && computer === 'paper'){
-  return 'you win';
+  }else if(user === "Rock" && computer === "Scissors"){
+  return "you win!";
+
+}else if (user === "Paper" && computer === "Rock"){
+  return "you win!" ;
+}else if(user === "Scissors" && computer === "Paper"){
+  return "you win!";
 }else{
-  return 'you loose';
+  return "you lose!";
 }
-
-
+}
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -205,10 +204,12 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+      for (let i =number; i >0;i--){
+       return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-i} bottles of soda on the wall`;
+      }
 }
-
+console.log('task 6', annoyingSong(50));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
